9ceb74be42ab263faf082dfa86236e1c54cbf3f5
Tamuga23 <129322024+Tamuga23@users.noreply.github.com>
2025-11-20 18:40:47 -0600

    Actualizando lista de productos

--- constants.py
+++ constants.py
@@ -11,11 +11,31 @@ PANDA_STORE_INFO = {
 
 # Catálogo de productos (Simplificado del tuyo)
 PRODUCT_CATALOG = [
-  {"id": "1001", "description": "Xiaomi Mi Band 8"},
+{"id": "1001", "description": "Xiaomi Mi Band 8"},
   {"id": "1002", "description": "Xiaomi Mi TV Box S, 2a Generación"},
   {"id": "1004", "description": "Amazon Fire TV HD"},
   {"id": "1014", "description": "Amazon Fire TV Stick 4K"},
   {"id": "1015", "description": "Echo Dot 5ta Generación"},
+  {"id": "1013", "description": "Xiaomi Redmi Watch 4"},
+  {"id": "1025", "description": "CeraVe SA Cleanser 237ml"},
+  {"id": "1059", "description": "Xiaomi Mi Band 9"},
+  {"id": "1062", "description": "MagicCubic Proyector Portatil"},
+  {"id": "1079", "description": "Echo Spot with TP-Link Tapo Smart Color Bulb"},
+  {"id": "1080", "description": "Xiaomi Redmi Watch 5 Active"},
+  {"id": "1081", "description": "Xiaomi Redmi Watch 5 Lite"},
   {"id": "1110", "description": "Amazon Echo Show 5"},
+  {"id": "1112", "description": "MagicCubic Proyector Portatil L018 650 ANSI"},
+  {"id": "1114", "description": "Redmi Buds 6 Lite"},
+  {"id": "1097", "description": "70mai Dash Cam M310 1296P"},
+  {"id": "1132", "description": "Redmi Watch 5"},
+  {"id": "1138", "description": "Xiaomi Mi TV Box S, 3a Generación"},
+  {"id": "1139", "description": "TP-Link - Cámara de seguridad inalámbrica para exteriores, 1080P"},
+  {"id": "1142", "description": "MagicCubic Proyector Portatil HY450 900 ANSI"},
+  {"id": "1145", "description": "Amazfit Bip 6"},
+  {"id": "1153", "description": "Amazfit Active 2 Premium Version"},
+  {"id": "1154", "description": "Amazfit Active 2"},
+  {"id": "1164", "description": "MagicCubic Proyector Portatil HY310 330 ANSI"},
+  {"id": "1169", "description": "Xiaomi Mi Band 10"},
+  {"id": "1174", "description": "MagicCubic Proyector Portatil HY350MAX 900 ANSI"},
   # ... puedes agregar el resto de tu lista aquí
 ]
